4aff021eb2b6dcd6cbc78dcf9f1f5d1684ab2e86
kfreiberga2 <kfreiberga2@gmail.com>
2020-02-14 20:21:54 +0200

    salary and employee

---
+++ PaySystem/src/lv/kristine/employee/SalaryEmployee.java
@@ -0,0 +1,32 @@
+package lv.kristine.employee;
+
+import lv.kristine.paysystem.Payee;
+
+public class SalaryEmployee implements Payee {
+
+	private String name;
+	private Integer bankAccount;
+	protected Double grossWage;
+
+	public SalaryEmployee(String name, Integer bankAccount, Double grossWage) {
+		this.name = name;
+		this.bankAccount = bankAccount;
+		this.grossWage = grossWage;
+	}
+
+	@Override
+	public String name() {
+		return name;
+	}
+
+	@Override
+	public Double grossPayment() {
+		return grossWage;
+	}
+
+	@Override
+	public Integer bankAccount() {
+		return bankAccount;
+	}
+
+}
